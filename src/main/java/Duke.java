2f5c80d761ef3e9f42c2942bcaf28c640e74f0b5
darrenoje <darrenoje97@gmail.com>
2019-08-26 15:18:38 +0800

    Level-3: Mark As Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] array;
-        array = new String[100];
+        Task[] array;
+        array = new Task[100];
         int counter=0;
 
         String logo = " ____        _        \n"
@@ -14,22 +14,29 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you?");
         while (true) {
-            String words = sc.nextLine();
+            String words = sc.next();
             if (words.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
             if (words.equals("list")) {
-                System.out.println("list");
+                System.out.println("Here are the tasks in your list:");
                 for (int i=0; i<100; i++) {
                     if (array[i] == null) break;
-                    System.out.println(i+1 + "." + array[i]);
+                    System.out.println(i+1 + "." + "[" + array[i].getStatusIcon() + "] " + array[i].getDescription());
                 }
+            } else if (words.equals("done")) {
+                int num = sc.nextInt();
+                array[num-1].setDone(true);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + array[num-1].getStatusIcon() + "] " + array[num-1].getDescription());
             } else {
-                array[counter] = words;
+                String words2 = sc.nextLine();
+                String line = words + words2;
+                array[counter] = new Task(line);
                 counter++;
-                System.out.println("added: " + words);
+                System.out.println("added: " + line);
             }
 
         }
