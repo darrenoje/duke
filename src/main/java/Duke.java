431bdeed4af63d084eceb98a36d054dce5cf641c
darrenoje <darrenoje97@gmail.com>
2019-08-26 18:09:10 +0800

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Scanner sc = new Scanner(System.in);
         Task[] array;
         array = new Task[100];
@@ -14,55 +14,71 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you?");
         while (true) {
-            String words = sc.next();
-            if (words.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-
-            if (words.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i=0; i<100; i++) {
-                    if (array[i] == null) break;
-                    System.out.println(i+1 + "." + array[i].toString());
+            try {
+                String words = sc.next();
+                if (words.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
                 }
-            } else if (words.equals("done")) {
-                int num = sc.nextInt();
-                array[num-1].setDone(true);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + array[num-1].getStatusIcon() + "] " + array[num-1].getDescription());
-            } else if (words.equals("deadline")) {
-                String words2 = sc.nextLine();
-                String[] temparray = words2.trim().split(" /by ");
-                System.out.println("Got it. I've added this task:");
-                array[counter] = new Deadline(temparray[0], temparray[1]);
-                counter++;
-                System.out.println(array[counter-1].toString());
-                System.out.println("Now you have " + counter + " tasks in the list.");
-            } else if (words.equals("todo")) {
-                String words2 = sc.nextLine();
-                System.out.println("Got it. I've added this task:");
-                array[counter] = new Todo(words2.trim());
-                counter++;
-                System.out.println(array[counter-1].toString());
-                System.out.println("Now you have " + counter + " tasks in the list.");
-            } else if (words.equals("event")) {
-                String words2 = sc.nextLine();
-                String[] temparray = words2.trim().split(" /at ");
-                System.out.println("Got it. I've added this task:");
-                array[counter] = new Event(temparray[0], temparray[1]);
-                counter++;
-                System.out.println(array[counter-1].toString());
-                System.out.println("Now you have " + counter + " tasks in the list.");
-            }
-            else {
-                String words2 = sc.nextLine();
-                String line = words + words2;
-                array[counter] = new Task(line);
-                counter++;
-                System.out.println("added: " + line);
-            }
 
+                if (words.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < 100; i++) {
+                        if (array[i] == null) break;
+                        System.out.println(i + 1 + "." + array[i].toString());
+                    }
+                } else if (words.equals("done")) {
+                    int num = sc.nextInt();
+                    array[num - 1].setDone(true);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + array[num - 1].getStatusIcon() + "] " + array[num - 1].getDescription());
+                } else if (words.equals("deadline")) {
+                    try {
+                        String words2 = sc.nextLine();
+                        if (words2.trim().isEmpty())
+                            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        String[] temparray = words2.trim().split(" /by ");
+                        System.out.println("Got it. I've added this task:");
+                        array[counter] = new Deadline(temparray[0], temparray[1]);
+                        counter++;
+                        System.out.println(array[counter - 1].toString());
+                        System.out.println("Now you have " + counter + " tasks in the list.");
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else if (words.equals("todo")) {
+                    try {
+                        String words2 = sc.nextLine();
+                        if (words2.trim().isEmpty())
+                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                        System.out.println("Got it. I've added this task:");
+                        array[counter] = new Todo(words2.trim());
+                        counter++;
+                        System.out.println(array[counter - 1].toString());
+                        System.out.println("Now you have " + counter + " tasks in the list.");
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else if (words.equals("event")) {
+                    try {
+                        String words2 = sc.nextLine();
+                        if (words2.trim().isEmpty())
+                            throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+                        String[] temparray = words2.trim().split(" /at ");
+                        System.out.println("Got it. I've added this task:");
+                        array[counter] = new Event(temparray[0], temparray[1]);
+                        counter++;
+                        System.out.println(array[counter - 1].toString());
+                        System.out.println("Now you have " + counter + " tasks in the list.");
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
